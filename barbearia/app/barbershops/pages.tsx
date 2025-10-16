41928d8f3a1ffa5bc2ded7d9ac6428b4e8a4a262
Mariana Vargas <mariana_vargas@edu.univali.br>
2025-10-16 14:18:57 -0300

    feat: permitir que o usuário possa buscar por barbearias

---
+++ barbearia/app/barbershops/pages.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/app/_lib/prisma";
+import Header from "../components/ui/header";
+import BarbershopItem from "../components/ui/barbershop-item";
+import { redirect } from "next/navigation";
+import Search from "../components/ui/search";
+interface BarbeshopsPageProps {
+    searchParams: {
+        search?: string;
+    }
+}
+
+const BarbeshopsPage = async ({searchParams}: BarbeshopsPageProps) => {
+    if(!searchParams.search) {
+        return redirect("/")
+    }
+    const barbershops = await db.barbershop.findMany({
+        where: {
+            name: { 
+                contains: searchParams.search,
+                mode: 'insensitive'
+            } }
+    });
+    return <>
+        <Header />
+
+        <div className="px-5 py-6 flex flex-col gap-6">
+            <Search defaultValues={{ 
+                search: searchParams.search,
+                
+            }} 
+            />
+
+            <h1 className="text-gray-400 font-bold text-xs uppercase">
+                Resultados para &quot;{searchParams.search}&quot;
+            </h1>
+
+            <div className="grid grid-cols-2 gap-4">
+                {barbershops.map(barbershop => (
+                    <div key={barbershop.id} className="w-full">
+                        <BarbershopItem barbershop={barbershop} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    </>;
+};
+ 
+export default BarbeshopsPage;
